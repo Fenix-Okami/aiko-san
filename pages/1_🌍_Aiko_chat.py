a65341ff27a5bd17d8a243288919a48c599b921f
Fenix-Okami <raymundo.rojo46@gmail.com>
2023-11-25 02:52:59 +0000

    added reset button and improved asthetics of token counter

--- pages/1_🌍_Aiko_chat.py
+++ pages/1_🌍_Aiko_chat.py
@@ -80,13 +80,16 @@ n_level= st.sidebar.selectbox("Set Aiko's level",('N5 - Basic',
                                                   'N3 - Intermediate',
                                                   'N3 - Advanced',
                                                   'N1 - Fluent'))
-gpt_model= st.sidebar.selectbox("Set GPT model",('gpt-4','gpt-3.5-turbo'))
+gpt_model= st.sidebar.selectbox("Set GPT model",('gpt-4','gpt-3.5-turbo'),index=1)
+
+reset_button = st.sidebar.button("Reset Chat")
 
 with st.sidebar:
     st.write("""| Model                   | Input Cost per 1K Tokens | Output Cost per 1K Tokens |
 |-------------------------|--------------------------|---------------------------|
 | gpt-4                   | $0.030                  | $0.060                     |
 | gpt-3.5-turbo           | $0.001                  | $0.002                     |
+             
 """)
 
 
@@ -147,88 +150,88 @@ def get_audio(message):
 
 def main():
     st.title("Aiko-San")
-    
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = gpt_model
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    if reset_button:
+        st.session_state.messages = []
+        st.markdown('')
+ 
+        
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = gpt_model
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-if prompt := st.chat_input("Ask Aiko"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        system=[{"role":"system","content": f"""
-                 Persona: Japanese Tutor - Aiko. 
-                 Background: A college student based in Tokyo. 
-                 Personality Traits: Warm and welcoming, enthusiastic about teaching. 
-                 Tutoring Expertise: Proficient in tutoring Japanese language learners, 
-                    knowledgeable about Japanese culture, capable of teaching students
-                    at various proficiency levels (current student is at the {n_level}).
-                 Language Usage: Utilizes appropriate hiragana and kanji, emphasizes natural, 
-                    conversational responses. 
-                 Interaction Guidelines: Respond as a normal person would in a conversation, 
-                    tailor responses to the learner's proficiency level ({n_level}), 
-                    include cultural insights relevant to language learning. 
-                    Always correct the student's grammar, and provide the explaination in english.
-                    For a bit more of a human touch use italicized text in its own paragraph,
-                    describe Aiko performing some action, such as waving hello, smiling,
-                    or grabbing a cup of coffee. This is a roleplay so have Aiko play along.
-                    always refer to the student as "you" and never say "the student.
-                    Always perform this action first before responding.
-                    {immersion}
-                 Note: Aiko's responses should be engaging and supportive, encouraging 
-                    the learner's progress in understanding and speaking Japanese,
-                    while providing an immersive learning experience."
-                """}]
-        for response in client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=system+[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += (response.choices[0].delta.content or "")
-            message_placeholder.markdown(full_response + "▌")
+    if prompt := st.chat_input("Ask Aiko"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-            print(response)
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            system=[{"role":"system","content": f"""
+                    Persona: Japanese Tutor - Aiko. 
+                    Background: A college student based in Tokyo. 
+                    Personality Traits: Warm and welcoming, enthusiastic about teaching. 
+                    Tutoring Expertise: Proficient in tutoring Japanese language learners, 
+                        knowledgeable about Japanese culture, capable of teaching students
+                        at various proficiency levels (current student is at the {n_level}).
+                    Language Usage: Utilizes appropriate hiragana and kanji, emphasizes natural, 
+                        conversational responses. 
+                    Interaction Guidelines: Respond as a normal person would in a conversation, 
+                        tailor responses to the learner's proficiency level ({n_level}), 
+                        include cultural insights relevant to language learning. 
+                        Always correct the student's grammar, and provide the explaination in english.
+                        For a bit more of a human touch use italicized text in its own paragraph,
+                        describe Aiko performing some action, such as waving hello, smiling,
+                        or grabbing a cup of coffee. This is a roleplay so have Aiko play along.
+                        always reference the user as "you" and never say "the student".
+                        Always perform this action first before responding.
+                        {immersion}
+                    Note: Aiko's responses should be engaging and supportive, encouraging 
+                        the learner's progress in understanding and speaking Japanese,
+                        while providing an immersive learning experience."
+                    """}]
+            for response in client.chat.completions.create(
+                                                        model=st.session_state["openai_model"],
+                                                        messages=system+[
+                                                            {"role": m["role"], "content": m["content"]}
+                                                            for m in st.session_state.messages],
+                                                        stream=True,
+                                                        ):
+                full_response += (response.choices[0].delta.content or "")
+                message_placeholder.markdown(full_response + "▌")
 
-        # Initialize token count
+            ###Track token usage for awareness
+            messages =          system + [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
+            prompt_tokens =     num_tokens_from_messages(messages,gpt_model)
+            generated_tokens  = num_tokens_from_string(full_response,'cl100k_base')
+            token_usage =       prompt_tokens + generated_tokens
 
-        prompt_tokens = 0
-        messages = system + [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
-        # # Tokenize each message and add to the count
-        # for prompt_piece in messages:
-        #     tokens = num_tokens_from_string(prompt_piece['content'],gpt_model)
-        #     prompt_tokens += len(tokens)
-        prompt_tokens = num_tokens_from_messages(messages,gpt_model)
-        generated_tokens  =  num_tokens_from_string(full_response,'cl100k_base')
-        token_usage = prompt_tokens+generated_tokens
+            message_placeholder.markdown(full_response)
+            if voice_toggle:
+                # message_placeholder.audio(get_audio(full_response), format='audio/mp3')
+                st.audio(get_audio(full_response), format='audio/mp3')
+            if image_toggle:
+                # message_placeholder.image(get_image(full_response))
+                st.image(get_image(full_response))
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-        message_placeholder.markdown(full_response)
-        if voice_toggle:
-            # message_placeholder.audio(get_audio(full_response), format='audio/mp3')
-            st.audio(get_audio(full_response), format='audio/mp3')
-        if image_toggle:
-            # message_placeholder.image(get_image(full_response))
-            st.image(get_image(full_response))
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
-    
-    with st.sidebar:
-        st.write(f"{prompt_tokens} tokens in input")
-    with st.sidebar:
-        st.write(f"{generated_tokens} tokens generated in output")
-    with st.sidebar:
-        st.write(f"{token_usage} total tokens in chat. max context length is 8000")
+        with st.sidebar:
+            st.code(f"""
+                    input: {prompt_tokens} tokens
+                    output: {generated_tokens} tokens
+                    {token_usage}/8000 tokens in full context
+                    """)
+
+
+           
+
 
 if __name__ == "__main__":
     if not openai_api_key.startswith('sk-'):
